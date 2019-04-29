91643a1696cc20a9def44f6da74a5898516867a7
gunja <konara@ya.ru>
2019-04-29 15:31:33 +0300

    mistype fix

--- invoker.py
+++ invoker.py
@@ -5,7 +5,7 @@ import dbus
 """Proxy object from the Test object in receiver."""
 obj = dbus.SessionBus().get_object('sub.domain.tld', '/tld/domain/sub/Test')
 
-"""Call a method that simply retruns a string."""
+"""Call a method that simply returns a string."""
 print(obj.foo(dbus_interface='tld.domain.sub.TestInterface'))
 
 """Invoke a method that throws an exception and catch it."""
@@ -15,7 +15,7 @@ except Exception as e:
     print(str(e))
 
 """
-Call the stop method of the proxxied object which will stop the receivers
+Call the stop method of the proxied object which will stop the receivers
 main loop.
 """
 print(obj.stop(dbus_interface='tld.domain.sub.TestInterface'))
